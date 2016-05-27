5bead012f4a904a4f61798062e0a7cc31e13995c
Gustavo Fragoso <gbfragoso94@gmail.com>
2016-05-27 15:29:10 -0300

    Add files via upload
    Prolog files

---
+++ expr2rpn.pl
@@ -0,0 +1,20 @@
+expr --> termo,rexpr.
+rexpr --> [+],expr,{write('+'),write(' ')}.
+rexpr --> [-],expr,{write('-'),write(' ')}.
+rexpr --> [].
+termo --> fator,rtermo.
+rtermo --> [*],termo,{write('*'),write(' ')}.
+rtermo --> ['/'],termo,{write('/'),write(' ')}.
+rtermo --> [].
+fator --> [X],{integer(X)},{write(X),write(' ')}.
+fator --> ['('],expr,[')'].
+
+run(X) :- tell('expr.txt'),
+(
+	firstsolution(X),
+	fail;
+	told
+).
+
+firstsolution(X) :- (expr(X,[]) -> true;
+				false).
